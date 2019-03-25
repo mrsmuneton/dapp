18dca1653b84d06c74b3b93ce8d2d5cfbc742a3b
Ray <mrs.muneton@gmail.com>
2019-03-25 03:33:33 -0700

    Showing EthBlocks.latest.hash

--- imports/ui/components/hello/hello.js
+++ imports/ui/components/hello/hello.js
@@ -3,12 +3,17 @@ import './hello.html';
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+  EthBlocks.init();
 });
 
 Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
   },
+  currentBlock() {
+    console.log(EthBlocks.latest);
+      return EthBlocks.latest.hash;
+  }
 });
 
 Template.hello.events({
